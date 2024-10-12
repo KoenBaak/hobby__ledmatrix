4e5322a4aa3f5f333c373eb33439120ea7b444e6
Koen Baak <koen.baak@mn.nl>
2024-10-12 13:39:15 +0200

    maybe fix?

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from driver import MatrixDriver, Color
 
-driver = MatrixDriver(n_color_bits=4)
+driver = MatrixDriver(n_color_bits=2)
 colors = driver.color_manager
 all_blue = [[colors.relative(red=1.0, green=.4, blue=.6)] * driver.n_rows] * driver.n_cols
 driver.show_frame(all_blue)
